1ca6f37cb22ee31e0fb87a69a56b5e7d4d530d63
Tendry <temaneke@bocasay.com>
2017-10-20 11:28:01 +0300

    redirect after password change

---
+++ src/UserBundle/EventListener/UserSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace UserBundle\EventListener;
+
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class UserSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $router;
+
+    /**
+     * @var int
+     */
+    // private $tokenTtl;
+
+    /**
+     * ResettingListener constructor.
+     *
+     * @param UrlGeneratorInterface $router
+     * @param int                   $tokenTtl
+     */
+    public function __construct(UrlGeneratorInterface $router/*, $tokenTtl*/)
+    {
+        $this->router = $router;
+        // $this->tokenTtl = $tokenTtl;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::RESETTING_RESET_SUCCESS => 'onSuccessResettingReset',
+        );
+    }
+
+    /**
+     * @param GetResponseUserEvent $event
+     */
+    public function onSuccessResettingReset(FormEvent $event)
+    {   
+        $event->setResponse(new RedirectResponse($this->router->generate('admin_dashboard_kpi')));
+    }
+}
