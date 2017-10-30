83af9adaa97f19ce6a8225da6d9b47275861f5b8
Anthony <andrana.anthony@gmail.com>
2017-10-30 16:24:04 +0300

    intégration formulaire de connexion

--- web/js/login.js
+++ web/js/login.js
@@ -1,52 +1,17 @@
 
 jQuery(document).ready(function() {
 	
-    /*
-        Fullscreen background
-    */
-    $.backstretch("assets/img/backgrounds/1.jpg");
-    
-    /*
-        Login form validation
-    */
-    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
-    	$(this).removeClass('input-error');
-    });
-    
-    $('.login-form').on('submit', function(e) {
-    	
-    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
-    		if( $(this).val() == "" ) {
-    			e.preventDefault();
-    			$(this).addClass('input-error');
-    		}
-    		else {
-    			$(this).removeClass('input-error');
-    		}
-    	});
-    	
-    });
-    
-    /*
-        Registration form validation
-    */
-    $('.registration-form input[type="text"], .registration-form textarea').on('focus', function() {
-    	$(this).removeClass('input-error');
-    });
-    
-    $('.registration-form').on('submit', function(e) {
-    	
-    	$(this).find('input[type="text"], textarea').each(function(){
-    		if( $(this).val() == "" ) {
-    			e.preventDefault();
-    			$(this).addClass('input-error');
-    		}
-    		else {
-    			$(this).removeClass('input-error');
-    		}
-    	});
-    	
-    });
-    
-    
+	jQuery('span#checked-unchecked-radio-souvenir').on('click', function(){
+		$(this).removeClass('checked-souvenir');
+		$(this).removeClass('check-souvenir');
+		if($('input#remember_me').prop('checked')){
+			$('input#remember_me').removeAttr('checked');
+			$(this).addClass('check-souvenir');
+			$(this).removeClass('checked-souvenir');
+		}else{
+			$('input#remember_me').attr('checked', true);
+			$(this).removeClass('check-souvenir');
+			$(this).addClass('checked-souvenir');
+		}
+	});
 });
