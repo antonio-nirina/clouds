3090fb929a9e5588459f5a2d062203ac5361b693
Mirija <mandriantsilaizina@bocasay.com>
2017-11-28 17:11:12 +0300

    Beneficiary-Home page : getting editorial content

--- src/BeneficiaryBundle/Controller/PageController.php
+++ src/BeneficiaryBundle/Controller/PageController.php
@@ -12,6 +12,23 @@ class PageController extends Controller
      */
     public function homePageAction()
     {
-        return $this->render('BeneficiaryBundle:Page:home.html.twig');
+        $program = $this->container->get('admin.program')->getCurrent();
+        if (empty($program)) {
+            return $this->redirectToRoute('fos_user_security_logout');
+        }
+
+        $home_page_data = $program->getHomePageData();
+        if (is_null($home_page_data)) {
+            return $this->redirectToRoute('fos_user_security_logout');
+        }
+
+        $editorial = $home_page_data->getEditorial();
+        if (is_null($editorial)) {
+            return $this->redirectToRoute('fos_user_security_logout');
+        }
+
+        return $this->render('BeneficiaryBundle:Page:home.html.twig', array(
+            'editorial' => $editorial,
+        ));
     }
 }
