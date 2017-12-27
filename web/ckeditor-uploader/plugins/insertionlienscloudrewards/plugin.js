c881e9aafa8f24890136d76b25d63aeb2c0fe97f
Anthony <andrana.anthony@gmail.com>
2017-12-27 16:58:49 +0300

    popUp ajout lien

--- web/ckeditor-uploader/plugins/insertionlienscloudrewards/plugin.js
+++ web/ckeditor-uploader/plugins/insertionlienscloudrewards/plugin.js
@@ -12,25 +12,47 @@ CKEDITOR.plugins.add( 'insertionlienscloudrewards', {
 				//Ouvre le popUp
 				$('#conteneur-popup').show();
 				$('#body-popup').removeAttr('style');
-				$('#body-popup').attr('style', 'width:40%;margin-left:-20%;');
+				$('#body-popup').attr('style', 'width:60%;margin-left:-30%;height:auto;');
 				$('#body-popup').show();
 
 				
 				var HTML = '';
-				HTML += '<h3 class="titrePopUp"><span id="fermerPopUp">X</span></h3>';
+				HTML += '<h3 class="titrePopUp">créer un lien<span id="fermerPopUp">X</span></h3>';
 				HTML += '<div class = "conteneur-form-insert-img-ckeditor">';
-				HTML += '<div class="titre-section-page-standard"><span>Ajouté un lien</span></div>';
+				//HTML += '<div class="titre-section-page-standard"><span>créer un lien</span></div>';
 				HTML += '<form class = "formAjoutLienPopUp" name = "ajouterLien" method = "POST" action = "">';
+				
+				HTML += '<div class = "clearBoth"></div>';
+				
 				HTML += '<label class="champForm">';
-				HTML += '<select id = "url-type-popup" class = "select-form" name = "typeUrl">';
-				HTML += '<option value = "">Url</option>';
-				HTML += '<option value = "http://">http://</option>';
-				HTML += '<option value = "https://">https://</option>';
-				HTML += '<option value = "mailto:">mailto:</option>';
-				HTML += '</select>';
-				HTML += '<input id = "url-valeur-popup" style="padding-left:10px!important;" class="input-form-text-pop" name="url" value="" placeholder="" type="text">';
+				HTML += '<span class = "lib-form">url</span>';
+				HTML += '<input class = "input-form-text" type = "text" name = "url" value = "http://" style = "padding-left:10px!important;width:80%;">';
+				HTML += '<span class="delete-input"></span>';
+				HTML += '<p style = "clear: both;width: 80%;float: right;margin-right: 33px;">le lien s\'ouvrira dans un nouvel onglet du navigateur</p>';
+				HTML += '</label>';
+				
+				HTML += '<label class="champForm" style = "clear:both;">';
+				HTML += '<span>ou</span>';
+				HTML += '<hr style = "display: inline-block;height: 2px;background: #efefef;width: 92%;vertical-align: middle;text-align: right;margin-left: 1%;clear: right;">';
+				HTML += '</label>';
+				
+				HTML += '<label class="champForm">';
+				HTML += '<span class = "lib-form">mailto</span>';
+				HTML += '<input class = "input-form-text" type = "text" name = "mailto" style = "padding-left:10px!important;width:80%;" placeholder = "service_client@cloudrewards.com">';
 				HTML += '<span class="delete-input"></span>';
 				HTML += '</label>';
+				
+				HTML += '<div class = "clearBoth"></div>';
+				HTML += '<div class = "clearBoth"></div>';
+				HTML += '<div class = "clearBoth"></div>';
+				
+				HTML += '<label class="champForm">';
+				HTML += '<span class = "lib-form">texte à afficher</span>';
+				HTML += '<input class = "input-form-text" type = "text" name = "texte_afficher" style = "padding-left:10px!important;width:80%;" placeholder = "service_client@cloudrewards.com">';
+				HTML += '<span class="delete-input"></span>';
+				HTML += '</label>';
+				
+				HTML += '<div class = "clearBoth"></div>';
 				
 				HTML += '<label class="champForm">';
 				HTML += '<button id = "submit-form-ajout-url" class="btn-valider valider submit-form block-center" type = "button">';
@@ -56,6 +78,10 @@ CKEDITOR.plugins.add( 'insertionlienscloudrewards', {
 });
 
 $(document).ready(function(){
+	$(document).on('click', 'span.delete-input', function(){
+		$(this).prev('input[type="text"]').val('');
+	});
+	
 	$(document).on('click', 'button#submit-form-ajout-url', function(){
 		var UrlVal = $('input#url-valeur-popup').val();
 		var UrlType = $('select#url-type-popup').val();
--- web/ckeditor/custom/config-header-declaration.js
+++ web/ckeditor/custom/config-header-declaration.js
@@ -23,5 +23,7 @@ CKEDITOR.editorConfig = function( config ) {
     ];
 
     config.removeButtons = 'Source,Save,NewPage,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Undo,Redo,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Underline,Strike,Subscript,Superscript,CopyFormatting,RemoveFormat,Outdent,Indent,CreateDiv,BidiLtr,BidiRtl,Language,Link,Unlink,Anchor,Table,Flash,Image,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,FontSize,Font,Format,Styles,TextColor,BGColor,ShowBlocks,Maximize,About';
+	config.removePlugins = 'elementspath';
+	config.extraPlugins = 'insertionlienscloudrewards';
     // config.height = 300;
 };
