a79fb4e314c8db7f60be065d3fabed87a5e1b332
Mirija <mandriantsilaizina@bocasay.com>
2018-01-09 14:42:55 +0300

    Admin-Communication-Emailing-Templates : showing timer whene doing ajax call

--- web/js/admin-communication.js
+++ web/js/admin-communication.js
@@ -172,6 +172,7 @@ $(document).ready(function(){
         if(null !== template_model){
             var add_template_url = $('input[name=add_template_form_url]').val();
             add_template_url = add_template_url+'/'+template_model;
+            $('.chargementAjax').removeClass('hidden');
             $.ajaxSetup({async: false});
             $.ajax({
                 type: 'GET',
@@ -194,6 +195,7 @@ $(document).ready(function(){
                 });
             }, 0);
             $.ajaxSetup({async: true});
+            $('.chargementAjax').addClass('hidden');
         }
     });
 
@@ -258,6 +260,7 @@ $(document).ready(function(){
             CKEDITOR.instances[name].updateElement();
         }
 
+        $('.chargementAjax').removeClass('hidden');
         $('#create-template-dialog form').ajaxSubmit({
             type: 'POST',
             url: add_template_url,
@@ -279,6 +282,7 @@ $(document).ready(function(){
                 }
             }
         });
+        $('.chargementAjax').addClass('hidden');
     });
 
     /**
@@ -494,6 +498,7 @@ $(document).ready(function(){
      */
     $(document).on('click', '.edit-template', function(e){
         e.preventDefault();
+        $('.chargementAjax').removeClass('hidden');
         $.ajax({
             type: 'GET',
             url: $(this).attr('data-target-url'),
@@ -502,6 +507,7 @@ $(document).ready(function(){
                 $('#create-template-dialog').find('a.previous').hide();
                 $('#create-template-dialog').find('.error-message-container.general-message').text('');
                 $('#create-template-dialog').modal('show');
+                $('.chargementAjax').addClass('hidden');
             },
             statusCode: {
                 404: function(data){
@@ -509,6 +515,7 @@ $(document).ready(function(){
                     $('#create-template-dialog').find('.error-message-container.general-message').text(data.responseJSON.message);
                     $('#create-template-dialog').find('.modal-body-container').html('');
                     $('#create-template-dialog').modal('show');
+                    $('.chargementAjax').addClass('hidden');
                 }
             }
         });
@@ -520,6 +527,7 @@ $(document).ready(function(){
         for (name in CKEDITOR.instances) {
             CKEDITOR.instances[name].updateElement();
         }
+        $('.chargementAjax').removeClass('hidden');
         $('#create-template-dialog form').ajaxSubmit({
             type: 'POST',
             url: $(this).attr('data-target-url'),
@@ -529,14 +537,17 @@ $(document).ready(function(){
                     $('#create-template-dialog').find('.btn-valider.save').trigger('click');
                     installColorPicker();
                     installWysiwyg();
+                    $('.chargementAjax').addClass('hidden');
                 } else {
                     window.location.replace($('input[name=template_list_url]').val());
+                    $('.chargementAjax').addClass('hidden');
                 }
             },
             statusCode: {
                 404: function(data){
                     $('#create-template-dialog').find('.error-message-container.general-message').text('Erreur');
                     $('#create-template-dialog').find('.modal-body-container').html('');
+                    $('.chargementAjax').addClass('hidden');
                 }
             }
         });
@@ -608,6 +619,7 @@ $(document).ready(function(){
      */
     $(document).on('click', '.preview-template', function(e){
         e.preventDefault();
+        $('.chargementAjax').removeClass('hidden');
         $.ajax({
             type: 'GET',
             url: $(this).attr('data-target-url'),
@@ -615,17 +627,20 @@ $(document).ready(function(){
                 $('#preview-template-dialog').find('.modal-body-container').html(data.content);
                 $('#preview-template-dialog').find('.error-message-container.general-message').text('');
                 $('#preview-template-dialog').modal('show');
+                $('.chargementAjax').addClass('hidden');
             },
             statusCode:{
                 404: function(data){
                     $('#preview-template-dialog').find('.error-message-container.general-message').text(data.responseJSON.message);
                     $('#preview-template-dialog').find('.modal-body-container').html('');
                     $('#preview-template-dialog').modal('show');
+                    $('.chargementAjax').addClass('hidden');
                 },
                 500: function(){
                     $('#preview-template-dialog').find('.error-message-container.general-message').text('Erreur interne');
                     $('#preview-template-dialog').find('.modal-body-container').html('');
                     $('#preview-template-dialog').modal('show');
+                    $('.chargementAjax').addClass('hidden');
                 }
             }
         });
