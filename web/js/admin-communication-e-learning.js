6a0abaafdb5d0c1f4d37abb96cbf917318f2731e
Mirija <mandriantsilaizina@bocasay.com>
2018-03-15 16:43:05 +0300

    Admin-Communication-E-learning : design, create/edit e-learning (part 3)

--- web/js/admin-communication-e-learning.js
+++ web/js/admin-communication-e-learning.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
         e.preventDefault();
         $('#create-edit-e-learning-modal').modal('show');
         installWysiwyg();
+        installColorPicker()
     });
 
     /**
@@ -55,4 +56,28 @@ function installWysiwyg()
             customConfig: ckeditor_config_general_path
         });
     });
+}
+
+function installColorPicker()
+{
+    if ($('.color-value').length >0 ) {
+        $('.color-value').each( function() {
+            $(this).minicolors({
+                control: $(this).attr('data-control') || 'brightness',
+                defaultValue: $(this).attr('data-defaultValue') || '',
+                format: $(this).attr('data-format') || 'hex',
+                keywords: $(this).attr('data-keywords') || '',
+                inline: $(this).attr('data-inline') === 'true',
+                letterCase: $(this).attr('data-letterCase') || 'lowercase',
+                opacity: $(this).attr('data-opacity'),
+                position: $(this).attr('data-position') || 'bottom left',
+                swatches: $(this).attr('data-swatches') ? $(this).attr('data-swatches').split('|') : [],
+                change: function(value, opacity) {
+                    if( !value ) return;
+                    if( opacity ) value += ', ' + opacity;
+                },
+                theme: 'bootstrap'
+            });
+        });
+    }
 }
